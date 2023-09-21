f5954b31ee03474cbe5922b96c55f8380e745c45
Alex Stepanov <stepanov.hw@gmail.com>
2023-09-21 13:17:52 -0600

    create some finches in your database to be rendered on the page

--- main_app/views.py
+++ main_app/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 
-finches = [
-{'name': 'Finch1', 'species': 'house finch', 'color': 'red', 'age': 1, 'description': 'A common urban finch.'},
-    {'name': 'Finch2', 'species': 'goldfinch', 'color': 'yellow', 'age': 2, 'description': 'Known for its vibrant plumage.'},
-    {'name': 'Finch3', 'species': 'sparrow', 'color': 'brown', 'age': 2, 'description': 'Often found in city parks.'},
-    {'name': 'Finch4', 'species': 'zebra finch', 'color': 'gray', 'age': 1, 'description': 'Distinctive stripes on its throat.'},
-    {'name': 'Finch5', 'species': 'canary', 'color': 'yellow', 'age': 3, 'description': 'Popular pet bird with melodious songs.'},
-    {'name': 'Finch6', 'species': 'purple finch', 'color': 'purple', 'age': 2, 'description': 'Named for its plumage color.'},
-    {'name': 'Finch7', 'species': 'greenfinch', 'color': 'green', 'age': 1, 'description': 'Has a greenish hue.'},
-    {'name': 'Finch8', 'species': 'chaffinch', 'color': 'blue', 'age': 2, 'description': 'Known for its colorful markings.'},
-    {'name': 'Finch9', 'species': 'siskin', 'color': 'yellow-green', 'age': 1, 'description': 'Small and energetic.'},
-    {'name': 'Finch10', 'species': 'linnet', 'color': 'brown', 'age': 3, 'description': 'Recognizable by its brown plumage.'}
-]
+# Import the Cat Model
+from .models import Finch
+
+# finches = [
+# {'name': 'Finch1', 'species': 'house finch', 'color': 'red', 'age': 1, 'description': 'A common urban finch.'},
+#     {'name': 'Finch2', 'species': 'goldfinch', 'color': 'yellow', 'age': 2, 'description': 'Known for its vibrant plumage.'},
+#     {'name': 'Finch3', 'species': 'sparrow', 'color': 'brown', 'age': 2, 'description': 'Often found in city parks.'},
+#     {'name': 'Finch4', 'species': 'zebra finch', 'color': 'gray', 'age': 1, 'description': 'Distinctive stripes on its throat.'},
+#     {'name': 'Finch5', 'species': 'canary', 'color': 'yellow', 'age': 3, 'description': 'Popular pet bird with melodious songs.'},
+#     {'name': 'Finch6', 'species': 'purple finch', 'color': 'purple', 'age': 2, 'description': 'Named for its plumage color.'},
+#     {'name': 'Finch7', 'species': 'greenfinch', 'color': 'green', 'age': 1, 'description': 'Has a greenish hue.'},
+#     {'name': 'Finch8', 'species': 'chaffinch', 'color': 'blue', 'age': 2, 'description': 'Known for its colorful markings.'},
+#     {'name': 'Finch9', 'species': 'siskin', 'color': 'yellow-green', 'age': 1, 'description': 'Small and energetic.'},
+#     {'name': 'Finch10', 'species': 'linnet', 'color': 'brown', 'age': 3, 'description': 'Recognizable by its brown plumage.'}
+# ]
 
 # Create your views here.
 # Define the home view
@@ -23,6 +26,7 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {
         'finches': finches
     })
