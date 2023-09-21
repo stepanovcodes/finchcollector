7a577de718c98178eab2726e67e185c02d726006
Alex Stepanov <stepanov.hw@gmail.com>
2023-09-21 13:04:39 -0600

    Create a Finch model with at least three attributes

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+
+    # Changing instance method does not impact database, therefore no makemigrations is necessary 
+    def __str__(self):
+        return f'{self.name} ({self.id})'
